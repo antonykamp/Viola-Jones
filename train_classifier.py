992ea9342aa0acf846e6b7411c00d3a8c75a479d
Antony Kamp <tony.kamp@web.de>
2022-05-22 10:06:36 +0200

    add save classfier, collect vote of whole pic

--- train_classifier.py
+++ train_classifier.py
@@ -4,35 +4,45 @@ import violajones.AdaBoost as ab
 import violajones.Utils as utils
 import numpy as np
 import sys
+import pickle
 
 # sys.argv
 # 1 positive training npy files
 # 2 negative training npy files
+# 3 pickle file in/output
+# 4 positive testing npy files
+# 5 negative testing npy files
 # 3 output path
 if __name__ == "__main__":
     pos_training_path = sys.argv[1]
     neg_training_path = sys.argv[2]
-
+    pickle_file_path = sys.argv[3]
+    
     num_classifiers = 3
     # For performance reasons restricting feature size
     min_feature_height = 5
     max_feature_height = 9
     min_feature_width = 5
     max_feature_width = 9
+    if not os.path.exists(pickle_file_path):
+      print('Loading faces..') 
+      faces_ii_training = utils.load_images(pos_training_path)
+      print('..done. ' + str(len(faces_ii_training)) + ' faces loaded.\n\nLoading non faces..')
+      non_faces_ii_training = utils.load_images(neg_training_path)
+      print('..done. ' + str(len(non_faces_ii_training)) + ' non faces loaded.\n')
 
-    print('Loading faces..') 
-    faces_ii_training = utils.load_images(pos_training_path)
-    print('..done. ' + str(len(faces_ii_training)) + ' faces loaded.\n\nLoading non faces..')
-    non_faces_ii_training = utils.load_images(neg_training_path)
-    print('..done. ' + str(len(non_faces_ii_training)) + ' non faces loaded.\n')
-
-    # classifiers are haar like features
-    classifiers = ab.learn(faces_ii_training, non_faces_ii_training, num_classifiers, min_feature_height, max_feature_height, min_feature_width, max_feature_width)
+      # classifiers are haar like features
+      classifiers = ab.learn(faces_ii_training, non_faces_ii_training, num_classifiers, min_feature_height, max_feature_height, min_feature_width, max_feature_width)
+      with open(pickle_file_path,"wb") as filehandler:
+        pickle.dump(classifiers,filehandler)
+    else:
+      with open(pickle_file_path,"rb") as file:
+        classifiers = pickle.load(file)
     
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
       exit()
-    pos_testing_path = sys.argv[3]
-    neg_testing_path = sys.argv[4]
+    pos_testing_path = sys.argv[4]
+    neg_testing_path = sys.argv[5]
     print('Loading test faces..')
     faces_testing = utils.load_images(pos_testing_path)
     print('..done. ' + str(len(faces_testing)) + ' faces loaded.\n\nLoading test non faces..')
@@ -42,8 +52,8 @@ if __name__ == "__main__":
     print('Testing selected classifiers..')
     correct_faces = 0
     correct_non_faces = 0
-    correct_faces = sum(utils.ensemble_vote_all(faces_testing, classifiers))
-    correct_non_faces = len(non_faces_testing) - sum(utils.ensemble_vote_all(non_faces_testing, classifiers))
+    correct_faces = sum(utils.ensemble_all_vote_all(faces_testing, classifiers))
+    correct_non_faces = len(non_faces_testing) - sum(utils.ensemble_all_vote_all(non_faces_testing, classifiers))
 
     print('..done.\n\nResult:\n      Faces: ' + str(correct_faces) + '/' + str(len(faces_testing))
           + '  (' + str((float(correct_faces) / len(faces_testing)) * 100) + '%)\n  non-Faces: '
